139de51e83c93f87f6ba905a89beea98fb957382
SuveshBaskar <suvesh@yellowmessenger.com>
2020-01-11 01:31:09 +0530

    Fix Server JS

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello from Server")
+console.log("Hello from Server");
+
+let time = new Date().getTime();
+
+if((time%10)%2 == 0){
+	console.log("EVEN")
+} else {
+	console.log("ODD")
+}
